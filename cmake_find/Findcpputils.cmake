a8f935dcc04e0e339c619053f6ab9f9b7f1c826a
itsme <itsme@xs4all.nl>
2022-11-28 12:23:23 +0100

    added cmake finders for cpputils and hexdumper

---
+++ cmake_find/Findcpputils.cmake
@@ -0,0 +1,17 @@
+# NOTE: you can avoid downloading cpputils, by symlinking to a downloaded version here:
+find_path(CPPUTILS_DIR NAMES string-lineenum.h PATHS symlinks/cpputils)
+if(CPPUTILS_DIR STREQUAL "CPPUTILS_DIR-NOTFOUND")
+    include(FetchContent)
+    FetchContent_Populate(cpputils
+        GIT_REPOSITORY https://github.com/nlitsme/cpputils)
+    set(CPPUTILS_DIR ${cpputils_SOURCE_DIR})
+else()
+    set(cpputils_BINARY_DIR ${CMAKE_BINARY_DIR}/cpputils-build)
+endif()
+
+add_library(cpputils INTERFACE)
+target_include_directories(cpputils INTERFACE ${CPPUTILS_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(cpputils REQUIRED_VARS CPPUTILS_DIR)
+
---
+++ cmake_find/Findhexdumper.cmake
@@ -0,0 +1,16 @@
+find_path(HEXDUMPER_DIR NAMES dump_ossl_hash.h PATHS symlinks/hexdumper)
+if(HEXDUMPER_DIR STREQUAL "HEXDUMPER_DIR-NOTFOUND")
+    include(FetchContent)
+    FetchContent_Populate(hexdumper
+        GIT_REPOSITORY https://github.com/nlitsme/hexdumper)
+    set(HEXDUMPER_DIR ${hexdumper_SOURCE_DIR})
+else()
+    set(hexdumper_BINARY_DIR ${CMAKE_BINARY_DIR}/hexdumper-build)
+endif()
+
+add_library(hexdumper INTERFACE)
+target_include_directories(hexdumper INTERFACE ${HEXDUMPER_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(hexdumper REQUIRED_VARS HEXDUMPER_DIR)
+
